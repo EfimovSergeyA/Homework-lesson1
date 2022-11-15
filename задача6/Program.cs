e2087c0ef1d873035f1308db4c5e574621a2db6c
Сергей Ефимов <Serejaefimov1988@yandex.ru>
2022-11-15 19:19:47 +0300

    My homework

---
+++ задача6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число 1 ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2==1)
+{
+    Console.WriteLine("число нечетное");
+           
+}
+else
+ {
+     Console.WriteLine("Число четное");
+ }
